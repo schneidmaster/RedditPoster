490dae132a33705b1bd32493c1226913ed5c54cb
Zach Schneider <schneidmaster@gmail.com>
2015-05-30 23:47:14 -0400

    Move post task to Rakefile

--- config/schedule.example.rb
+++ config/schedule.example.rb
@@ -1,12 +1,25 @@
 # Examples of post schedules you can configure
 # See the whenever docs for more: https://github.com/javan/whenever
+#
+# Options:
+# append_date - determines whether to append the current date to the post title
+# distinguish - determines whether to distinguish the post after it's made
+#               (you must make your bot a moderator for this to work)
 
 # Post on some day of the week.
 every :saturday, at: '12pm' do
-  runner "Bot.post!('Saturday Thread', 'saturday')"
+  title = 'Saturday Thread'
+  name = 'saturday'
+  append_date = false
+  distinguish = false
+  rake "rp:post[\"#{title}\",#{name},#{append_date},#{distinguish}]"
 end
 
-# Post every day, also distinguish and append date.
-every :saturday, at: '12pm' do
-  runner "Bot.post!('Daily Thread', 'daily', { append_date: true, distinguish: true })"
+# Post every day of the week
+every :day, at: '12pm' do
+  title = 'Saturday Thread'
+  name = 'saturday'
+  append_date = false
+  distinguish = false
+  rake "rp:post[\"#{title}\",#{name},#{append_date},#{distinguish}]"
 end
