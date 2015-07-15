09d778381cb9b568fd7f6b87c913e7115635906f
Zach Schneider <schneidmaster@gmail.com>
2015-07-15 00:10:25 -0500

    Bundle before executing whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ namespace :deploy do
   after :deploy, :update_cron do
     on roles(:app) do
       within release_path do
+        execute :bundle, :install
         execute :bundle, :exec, :whenever, '-w'
       end
     end
